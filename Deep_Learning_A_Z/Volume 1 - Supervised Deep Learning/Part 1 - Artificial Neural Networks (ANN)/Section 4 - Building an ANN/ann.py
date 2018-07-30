544ab0e4b0a2c8d24c1f9ced9e46e503b791c26a
Naz Khan <nazmuskhan@yahoo.com>
2018-07-30 18:11:57 -0400

    changed hidden layer

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -51,27 +51,13 @@ from keras.layers import Dense
 # Initialising the ANN
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+# adding the input layer and the first hidden layer
+# choosing the optimal number of hidden layer nodes is purely preference
+# a good rule of thumb is to create an average of input layers and output layer to decide on #
+# this customization is also known as paramater tuning
+# we will create six nodes
 
-# Adding the second hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units=6, kernel_initializer="uniform", activation="relu", input_dim=11))
 
-# Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
-
-# Part 3 - Making predictions and evaluating the model
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+# adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer="uniform", activation="relu"))
