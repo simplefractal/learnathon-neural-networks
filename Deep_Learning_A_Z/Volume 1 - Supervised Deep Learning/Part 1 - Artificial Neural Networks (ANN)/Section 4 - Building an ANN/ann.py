47159a7fb5a6c7a368d94fee3f9ecd084fb9cf75
Naz Khan <nazmuskhan@yahoo.com>
2018-07-30 19:03:54 -0400

    predicted customer exit from back correctly up to 86% of the time

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -1,7 +1,7 @@
 # Artificial Neural Network
 
 # Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+# pip install --upgrade --no-deps theano
 
 # Installing Tensorflow
 # pip install tensorflow
@@ -48,7 +48,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
+# Initializing the ANN
 classifier = Sequential()
 
 # adding the input layer and the first hidden layer
@@ -56,8 +56,33 @@ classifier = Sequential()
 # a good rule of thumb is to create an average of input layers and output layer to decide on #
 # this customization is also known as paramater tuning
 # we will create six nodes
-
+# relu is the activation function we chose(rectifier) because it is one of the most commonly used
 classifier.add(Dense(units=6, kernel_initializer="uniform", activation="relu", input_dim=11))
 
 # adding the second hidden layer
 classifier.add(Dense(units=6, kernel_initializer="uniform", activation="relu"))
+
+# adding the output layer
+# we must change activation function to sigmoid function - helps to get probabilities 
+# we only have binary output so sigmoid works here otherwise we will need to use softmax
+classifier.add(Dense(units=1, kernel_initializer="uniform", activation="sigmoid"))
+
+# compiling the ANN
+# applying stochastic gradient descent
+classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=['accuracy'])
+
+# fit ANN to training set
+# fit ANN with training sets - matrix of features, then dependent variable vector
+# no rule of thumb to choose batch_size and # of epochs
+classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+
+# Part 3 - Making predictions and evaluating the model
+
+# predicting the test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
